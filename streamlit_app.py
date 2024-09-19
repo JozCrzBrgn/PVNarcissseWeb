716b652bb8f98aa08af8441f879214baeb2bcad7
JosueCruzBarragan <ing.cruzbarragan@hotmail.com>
2024-09-19 15:24:37 -0600

    Prueba de navegacion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,32 @@
 import streamlit as st
 
-st.title("Up and Ready!")
+#? --- PAGE SETUP ---#
+inventarios_page = st.Page(
+    page="views/inventario.py",
+    title="Inventario",
+    icon=":material/inventory:",
+    default=True
+)
+
+project_1_page = st.Page(
+    page="views/mermas.py",
+    title="Mermas",
+    icon=":material/inventory_2:"
+)
+
+project_2_page = st.Page(
+    page="views/abonos.py",
+    title="Abonos",
+    icon=":material/smart_toy:"
+)
+
+#? --- NAVEGATION SETUP [WITH SECTIONS] ---#
+pg = st.navigation(
+    {
+        "Info": [inventarios_page],
+        "projects": [project_1_page, project_2_page],
+    }
+)
+
+#? --- RUN NAVEGATION ---#
+pg.run()
